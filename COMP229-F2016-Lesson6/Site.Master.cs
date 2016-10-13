bfcf12a9df0e08c5625bef808a11bfb2bba8a546
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-10-12 21:44:16 -0400

    updated main navigation

--- COMP229-F2016-Lesson6/Site.Master.cs
+++ COMP229-F2016-Lesson6/Site.Master.cs
@@ -6,13 +6,35 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace COMP229_F2016_Lesson6
 {
     public partial class Site : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Debug.WriteLine(Page.Title + " loaded...");
+            if(!IsPostBack)
+            {
+                // check if a user is logged in
+                if(HttpContext.Current.User.Identity.IsAuthenticated)
+                {
+                    // show the contoso content area
+                    ContosoPlaceHolder.Visible = true;
+                    PublicPlaceHolder.Visible = false;
+                } else
+                {
+                    // only show login and register
+                    ContosoPlaceHolder.Visible = false;
+                    PublicPlaceHolder.Visible = true;
+                }
+
+            }
+
+
             SetActivePage();
         }
 
@@ -31,9 +53,6 @@ namespace COMP229_F2016_Lesson6
                     break;
                 case "Departments":
                     departments.Attributes.Add("class", "active");
-                    break;
-                case "Enrollments":
-                    enrollments.Attributes.Add("class", "active");
                     break;
                 case "About":
                     about.Attributes.Add("class", "active");
